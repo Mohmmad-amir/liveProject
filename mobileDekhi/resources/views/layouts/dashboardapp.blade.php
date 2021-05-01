79d8d9b22d1162c4130aa40668d7e19835dbf1ec
Utshab Hossain <utshab1999@gmail.com>
2021-05-01 13:23:17 +0600

    dashboard home

--- mobileDekhi/resources/views/layouts/dashboardapp.blade.php
+++ mobileDekhi/resources/views/layouts/dashboardapp.blade.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
 </head>
-<body>
+<body lass="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div id="app">
         <div class="warapper">
 
