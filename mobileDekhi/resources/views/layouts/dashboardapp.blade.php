454c62d12ffffe34aa97a9a5ab050694502be533
Utshab Hossain <utshab1999@gmail.com>
2021-05-01 14:06:53 +0600

    dashboard nav change

--- mobileDekhi/resources/views/layouts/dashboardapp.blade.php
+++ mobileDekhi/resources/views/layouts/dashboardapp.blade.php
@@ -19,7 +19,6 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{asset('assets/css/adminPanaleApp.css')}}">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/adminPanale.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
@@ -32,9 +31,9 @@
             <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="{{route('home')}}" class="brand-link">
+        <a style="text-decoration: none;" href="{{route('home')}}" class="brand-link">
             <img src="{{asset('assets/img/AdminLTELogo.png')}}"" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">MobileDekhi</span>
+            <span class="font-weight-light">MobileDekhi</span>
         </a>
 
         <!-- Sidebar -->
@@ -45,7 +44,7 @@
                     <img src="{{asset('assets/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                    <a style="text-decoration: none;" href="#" class="d-block">{{ Auth::user()->name }}</a>
                 </div>
             </div>
 
@@ -172,7 +171,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
