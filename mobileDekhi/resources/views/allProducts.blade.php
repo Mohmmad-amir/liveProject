dd7df11b009f23f474d804c4028ddf7cf6c0d8bd
Utshab Hossain <utshab1999@gmail.com>
2021-05-03 05:58:18 +0600

    add product

---
+++ mobileDekhi/resources/views/allProducts.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.dashboardapp')
+
+@section('content')
+<div class="warapper">
+  <div class="content-wrapper">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class=" col-md-10">
+          <div class="container  py-5">
+            <h2 class="text-center">
+              All Product
+            </h2>
+            @if (session('message'))
+                <div class="alert alert-success">
+                    <p>{{ session('message') }}</p>
+                </div>
+            @endif
+          
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Model</th>
+                <th colspan="2">Price</th>
+                <th colspan="2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {{-- @foreach ($subCarousels as $subCarousel) --}}
+              <tr>
+                <th scope="row"></th>
+                <td><img class="" style="width: 80px"
+                  src="" alt="">
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+          </td>
+                <td><a class="btn btn-info" href=""> <i class="fas fa-edit"></i> Edit </a></td>
+                <td>
+                  <form action="" method="POST" >
+                    @csrf
+                    @method("DELETE")
+                    <button class="btn btn-danger"> <i class="fas fa-trash"></i> Delete </button>
+                  </form>
+                </td>
+              </tr>
+              {{-- @endforeach --}}
+
+            </tbody>
+          </table>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
+
