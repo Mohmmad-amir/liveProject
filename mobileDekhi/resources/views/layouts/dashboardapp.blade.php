3d942a3ad0e349053b9ed74fba3229ff91953dcc
Mohmmad-amir <76723565+Mohmmad-amir@users.noreply.github.com>
2021-05-03 12:00:05 +0600

    ghange

--- mobileDekhi/resources/views/layouts/dashboardapp.blade.php
+++ mobileDekhi/resources/views/layouts/dashboardapp.blade.php
@@ -88,7 +88,7 @@
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="/addProduct" class="nav-link"> <i class="far fa-circle nav-icon"></i>
+                                        <a href="{{route('products.add')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
                                             <p>Add Product</p>
                                         </a>
                                     </li>
