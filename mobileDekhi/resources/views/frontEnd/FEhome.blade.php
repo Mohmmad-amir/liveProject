805468a06303076f22a39b995232a88526b12e58
Utshab Hossain <utshab1999@gmail.com>
2021-05-07 18:32:58 +0600

    change

--- mobileDekhi/resources/views/frontEnd/FEhome.blade.php
+++ mobileDekhi/resources/views/frontEnd/FEhome.blade.php
@@ -6,12 +6,12 @@
 
     <!-- carosal start -->
     {{$key=0}}
-    <div class="mt-4" id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
         <div class="carousel-inner">
             @foreach($mainCarousels as $mainCarosul)
 
@@ -19,7 +19,7 @@
                 {{-- <a href="#">
                     <img style="height:600px;" src="{{asset('assets/img')}}/{{$mainCarosul->image}}" class="d-block w-100" alt="...">
                 </a> --}}
-                <img style="height:700px;" src="{{asset('assets/img')}}/{{$mainCarosul->image}}" class="d-block w-100" alt="...">
+                <img style="height:750px;" src="{{asset('assets/img')}}/{{$mainCarosul->image}}" class="d-block w-100" alt="...">
 
             </div>
 
