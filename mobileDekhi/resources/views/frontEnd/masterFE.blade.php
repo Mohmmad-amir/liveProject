8c3247a3e0dc79edbfccafb1fafb0669d427c896
Manik <AnonymousManik@github.com>
2021-05-09 22:37:48 +0600

    change navbar

--- mobileDekhi/resources/views/frontEnd/masterFE.blade.php
+++ mobileDekhi/resources/views/frontEnd/masterFE.blade.php
@@ -14,10 +14,10 @@
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
         crossorigin="anonymous" />
     <!-- custom css -->
-    <link rel="stylesheet" href="{{asset('assets/css/FEstyle.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/FEhome.css')}}">
-    <link rel="stylesheet" href="{{asset('Carousel/dist/assets/owl.carousel.css')}}">
-    <link rel="stylesheet" href="{{asset('Carousel/dist/assets/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/FEstyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/FEhome.css') }}">
+    <link rel="stylesheet" href="{{ asset('Carousel/dist/assets/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('Carousel/dist/assets/owl.theme.default.min.css') }}">
 
     <link rel="stylesheet" href="mediumScreen.css">
     <link rel="stylesheet" href="smScreen.css">
@@ -28,8 +28,8 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg--color fixed-top  ">
         <div class="container">
-            <a class="navbar-brand" href="{{route('mainCarosul')}}"><strong class="nav-header-color">M</strong>obile<strong
-                    class="nav-header-color">D</strong>ekhi</a>
+            <a class="navbar-brand" href="{{ route('mainCarosul') }}"><strong
+                    class="nav-header-color">M</strong>obile<strong class="nav-header-color">D</strong>ekhi</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -38,59 +38,55 @@
             <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
                 <ul class="navbar-nav me-3 mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link {{Request()->Route()->named('mainCarosul')? 'active': ''}}" aria-current="page" href="{{route('mainCarosul')}}">Home</a>
+                        <a class="nav-link {{ Request()->Route()->named('mainCarosul')
+    ? 'active'
+    : '' }}"
+                            aria-current="page" href="{{ route('mainCarosul') }}">Home</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle  href="#" id="navbarDropdown" role="button"
+                        <a class="nav-link dropdown-toggle  href=" #" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             Brand
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{route('brand','samsung')}}">Samsung</a></li>
-                            <li><a class="dropdown-item" href="{{route('brand','nokia')}}">Nokia</a></li>
-                            <li><a class="dropdown-item" href="{{route('brand','Oneplus')}}">Oneplus</a></li>
-
-                            <li><a class="dropdown-item" href="{{route('brand','Oneplus')}}">Oneplus</a></li>
+                            <li><a class="dropdown-item" href="{{ route('brand', 'Samsung') }}">Samsung</a></li>
+                            <li><a class="dropdown-item" href="{{ route('brand', 'Nokia') }}">Nokia</a></li>
+                            <li><a class="dropdown-item" href="{{ route('brand', 'Oneplus') }}">Oneplus</a></li>
+                            <li><a class="dropdown-item" href="{{ route('brand', 'Apple') }}">Apple</a></li>
+                            <li><a class="dropdown-item" href="{{ route('brand', 'Xiaomi') }}">Xiaomi</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{Request()->Route()->named('filter.index')? 'active': ''}}" href="{{route('filter.index')}}">Filter</a>
+                        <a class="nav-link {{ Request()->Route()->named('filter.index') ? 'active': '' }}"
+                            href="{{ route('filter.index') }}">Filter</a>
                     </li>
-
-
-
                 </ul>
+                <form class="d-flex  " method="" action="{{ route('search') }}">
+                    <input required name="query" class="form-control searchInput" id="" type="search"
+                        placeholder="Search" aria-label="Search">
+                    <button class="btn text-white searchBarBtn" type="submit"><i class="fas fa-search"></i>
+                    </button>
+                </form>
 
-
-                <button id="searchBtn" class="btn text-white search-divider" type="submit"><i class="fas fa-search"></i>
-
-                </div>
-
+            </div>
         </div>
-
     </nav>
-    <div id="searchBar" class="searchBar w-100">
-        <form class="d-flex  " method="" action="{{route('search')}}">
-            <input required  name="query" class="form-control searchInput" id="" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn text-white searchBarBtn" type="submit"><i class="fas fa-search"></i>
-            </button>
-        </form>
-    </div>
-
-
-
-
-
-<main >
-@yield('content')
-</main>
 
 
 
 
 
 
-        <div class="bg--color">
+    <main>
+        @yield('content')
+    </main>
+
+
+
+
+
+
+    <div class="bg--color">
         <div class="container pt-4">
             <div class="row">
                 <div class="col-md-4 text-white ">
@@ -146,15 +142,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
         integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
         crossorigin="anonymous"></script>
-    <script src="{{asset('assets/js/navSearchBar.js')}}"></script>
+    <script src="{{ asset('assets/js/navSearchBar.js') }}"></script>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 
-        <script src="{{asset('Carousel/docs/assets/vendors/jquery.min.js')}}" type="text/javascript"></script>
-        <script src="{{asset('Carousel/dist/owl.carousel.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('Carousel/docs/assets/vendors/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('Carousel/dist/owl.carousel.js') }}" type="text/javascript"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
