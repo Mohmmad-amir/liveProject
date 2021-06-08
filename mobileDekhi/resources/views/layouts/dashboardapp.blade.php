3cacd0d0dd23352ed55c9b116e42dcdca36cb337
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2021-06-08 17:36:20 +0600

    upcoming link off

--- mobileDekhi/resources/views/layouts/dashboardapp.blade.php
+++ mobileDekhi/resources/views/layouts/dashboardapp.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,35 +18,44 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{asset('assets/css/adminPanaleApp.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/adminPanaleApp.css') }}">
 
     <link href="{{ asset('css/adminPanale.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" />
 
 </head>
+
 <body lass="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div id="app">
         <div class="warapper">
             <!-- Main Sidebar Container -->
             <aside style="position: fixed;" class="main-sidebar sidebar-dark-primary elevation-4">
-                <!-- Brand Logo --> <a style="text-decoration: none;" href="{{route('home')}}" class="brand-link">
-                <img src="{{asset('assets/img/Group.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span  class="font-weight-white"><span style="color: orangered;font-weight:bold" >M</span>obile<span style="color: orangered;font-weight:bold">D</span>ekhi</span>
+                <!-- Brand Logo --> <a style="text-decoration: none;" href="{{ route('home') }}" class="brand-link">
+                    <img src="{{ asset('assets/img/Group.png') }}" alt="AdminLTE Logo"
+                        class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <span class="font-weight-white"><span style="color: orangered;font-weight:bold">M</span>obile<span
+                            style="color: orangered;font-weight:bold">D</span>ekhi</span>
                 </a>
                 <!-- Sidebar -->
                 <div class="sidebar">
                     <!-- Sidebar user panel (optional) -->
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img src="{{asset('assets/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                            <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                                alt="User Image">
                         </div>
-                        <div class="info"> <a style="text-decoration: none;" href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <div class="info"> <a style="text-decoration: none;" href="#"
+                                class="d-block">{{ Auth::user()->name }}</a>
                         </div>
                     </div>
                     <!-- Sidebar Menu -->
                     <nav class="mt-2">
-                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                            data-accordion="false">
                             <!-- Add icons to the links using the .nav-icon class
                              with font-awesome or any other icon font library -->
                             {{-- <li class="nav-item">
@@ -68,7 +78,8 @@
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="{{route('mainCarousel.add')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
+                                        <a href="{{ route('mainCarousel.add') }}" class="nav-link"> <i
+                                                class="far fa-circle nav-icon"></i>
                                             <p>Main Carousel</p>
                                         </a>
                                     </li>
@@ -87,7 +98,7 @@
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview">
-                                    <li class="nav-item">
+                                    {{-- <li class="nav-item">
                                         <a href="{{route('upcomingProduct.add')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
                                             <p>Add Upcoming Product</p>
                                         </a>
@@ -96,14 +107,16 @@
                                         <a href="{{route('upcomingProduct')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
                                             <p>Upcoming Product</p>
                                         </a>
-                                    </li>
+                                    </li> --}}
                                     <li class="nav-item">
-                                        <a href="{{route('products.all')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
+                                        <a href="{{ route('products.all') }}" class="nav-link"> <i
+                                                class="far fa-circle nav-icon"></i>
                                             <p>All Product</p>
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="{{route('products.add')}}" class="nav-link"> <i class="far fa-circle nav-icon"></i>
+                                        <a href="{{ route('products.add') }}" class="nav-link"> <i
+                                                class="far fa-circle nav-icon"></i>
                                             <p>Add Product</p>
                                         </a>
                                     </li>
@@ -119,14 +132,18 @@
             <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm main-header fixed-top">
                 <div class="container">
                     <ul class="navbar-nav">
-                        <li class="nav-item"> <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                        <li class="nav-item"> <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                                    class="fas fa-bars"></i></a>
                         </li>
                         {{-- <li class="nav-item d-none d-sm-inline-block"> <a href="{{route('home')}}" class="nav-link">Home</a>
                         </li>
                         <li class="nav-item d-none d-sm-inline-block"> <a href="#" class="nav-link">Contact</a>
                         </li> --}}
                     </ul>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}"> <span class="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}"> <span
+                            class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
@@ -144,19 +161,23 @@
                                     </li>
                                 @endif
                             @else
-                            <li class="nav-item dropdown"> <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                                        <i style="margin-right: 5px; margin-left:25px;" class="left fas fa-power-off"></i>{{ __('Logout') }}
+                                <li class="nav-item dropdown"> <a id="navbarDropdown" class="nav-link dropdown-toggle"
+                                        href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                            <i style="margin-right: 5px; margin-left:25px;"
+                                                class="left fas fa-power-off"></i>{{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
                             @endguest
                         </ul>
                     </div>
@@ -170,25 +191,27 @@
 
 
 
-    <script src="{{asset('assets/js/jquery-3.5.1.min.js')}}"></script>
-<!-- Bootstrap -->
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<!-- overlayScrollbars -->
-<script src="{{asset('assets/js/jquery.overlayScrollbars.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('assets/js/adminlte.js')}}"></script>
-<!--custom js -->
-<script src="{{asset('assets/js/style.js')}}"></script>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- overlayScrollbars -->
+    <script src="{{ asset('assets/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('assets/js/adminlte.js') }}"></script>
+    <!--custom js -->
+    <script src="{{ asset('assets/js/style.js') }}"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 
-<script>
-    $(document).ready(function(){
-      $("#addMainCarousel").click(function(){
-        $("#showMainCarousel").fadeToggle();
-      });
-    });
+    <script>
+        $(document).ready(function() {
+            $("#addMainCarousel").click(function() {
+                $("#showMainCarousel").fadeToggle();
+            });
+        });
+
     </script>
 
 </body>
+
 </html>
