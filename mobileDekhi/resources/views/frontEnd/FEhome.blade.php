1d604b9b90762828c9b23ec383d83adad970a353
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2021-06-08 18:53:42 +0600

    add products

--- mobileDekhi/resources/views/frontEnd/FEhome.blade.php
+++ mobileDekhi/resources/views/frontEnd/FEhome.blade.php
@@ -104,7 +104,7 @@
 
     <h3 class="headingText mt-5 mb-5 text-center">Latest Mobiles Review</h3>
     <div class="container">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-md-4 mb-3">
                 <iframe width="100%" height="200" src="https://www.youtube.com/embed/1C07F3BgI_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
@@ -114,7 +114,7 @@
             <div class="col-md-4 mb-5">
                 <iframe width="100%" height="200" src="https://www.youtube.com/embed/VwaNOw2mHYI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-        </div>
+        </div> --}}
     </div>
 
     <!-- latest mobile review end -->
